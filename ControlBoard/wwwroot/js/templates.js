61d65725f2fc1c7cb50a3d4c52f3e56d3e898549
mihaijal <mihaijal@microsoft.com>
2018-12-01 00:48:11 -0800

    Style, panels etc.

--- ControlBoard/wwwroot/js/templates.js
+++ ControlBoard/wwwroot/js/templates.js
@@ -1,11 +1,12 @@
 ﻿function applyTemplates() {
     replaceSliders();
+    replacePanels();
 }
 
 function replaceSliders()
 {
     var nodes = document.getElementsByClassName("t-slider") 
-    var template = document.getElementById("t-slider");
+    var template = document.getElementsByClassName("slider-horizontal")[0];
     var count = nodes.length;
     for (var i = 0; i < count; i++) {
         
@@ -21,7 +22,7 @@ function replaceSliders()
     }
 
     var nodes = document.getElementsByClassName("t-slider-vert")
-    var template = document.getElementById("t-slider-vert");
+    var template = document.getElementsByClassName("slider-vertical")[0];
     var count = nodes.length;
     for (var i = 0; i < count; i++) {
         
@@ -29,11 +30,28 @@ function replaceSliders()
         var valueAsPercentage = (100 * (input.value - input.min) / (input.max - input.min)) + "%";
         var slider = template.cloneNode(true);
         slider.name = input.name;
-        slider.children[1].style.height = valueAsPercentage;
+        slider.children[0].style.height = valueAsPercentage;
         slider.children[1].innerHTML = input.value;
         slider.children[2].innerHTML = input.alt;
         copyClassesExcept(input, slider, "t-slider-vert");
         input.parentElement.replaceChild(slider, input);
+    }
+}
+
+function replacePanels()
+{
+    var nodes = document.getElementsByClassName("t-panel")
+    var template = document.getElementsByClassName("panel")[0];
+    var count = nodes.length;
+    for (var i = 0; i < count; i++) {
+        var node = nodes[i];
+        var panel = template.cloneNode(true);
+        var button = panel.children[0];
+        button.addEventListener("click", function () { toggleShow(this.nextElementSibling); });
+        button.insertBefore(document.createTextNode(node.title + " "), button.children[0]);
+        node.parentElement.insertBefore(panel, node);
+        panel.insertBefore(node, null);
+        node.classList.add("panel-body");
     }
 }
 
@@ -43,5 +61,14 @@ function copyClassesExcept(source, dest, exception) {
         if (classes[i] != exception) {
             dest.classList.add(classes[i]);
         }
+    }
+}
+
+function toggleShow(node)
+{
+    if (node.style.display != "none") {
+        node.style.display = "none";
+    } else {
+        node.style.display = "block";
     }
 }
