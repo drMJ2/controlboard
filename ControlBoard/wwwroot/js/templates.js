ccff022c9d19c648e536314a1ee25b9f55623e2f
Mihai Jalobeanu <mihaijal@microsoft.com>
2018-11-30 17:47:50 -0800

    prototype sliders

---
+++ ControlBoard/wwwroot/js/templates.js
@@ -0,0 +1,44 @@
+﻿function applyTemplates() {
+    replaceSliders();
+}
+
+function replaceSliders()
+{
+    var nodes = document.getElementsByClassName("t-slider") 
+    var template = document.getElementById("t-slider");
+    var count = nodes.length;
+    for (var i = 0; i < count; i++) {
+        
+        var input = nodes[0];
+        var valueAsPercentage = (100 * (input.value - input.min)/ (input.max - input.min)) + "%";
+        var slider = template.cloneNode(true);
+        slider.children[0].style.width = valueAsPercentage;
+        slider.children[0].innerHTML = input.alt;
+        slider.children[1].innerHTML = input.value;
+        copyClassesExcept(input, slider, "t-slider");
+        input.parentElement.replaceChild(slider, input);
+    }
+
+    var nodes = document.getElementsByClassName("t-slider-vert")
+    var template = document.getElementById("t-slider-vert");
+    var count = nodes.length;
+    for (var i = 0; i < count; i++) {
+        var input = nodes[0];
+        var valueAsPercentage = (100 * (input.value - input.min) / (input.max - input.min)) + "%";
+        var slider = template.cloneNode(true);
+        slider.children[1].style.height = valueAsPercentage;
+        slider.children[1].innerHTML = input.value;
+        slider.children[2].innerHTML = input.alt;
+        copyClassesExcept(input, slider, "t-slider-vert");
+        input.parentElement.replaceChild(slider, input);
+    }
+}
+
+function copyClassesExcept(source, dest, exception) {
+    var classes = source.classList;
+    for (var i = 0; i < classes.length; i++) {
+        if (classes[i] != exception) {
+            dest.classList.add(classes[i]);
+        }
+    }
+}
